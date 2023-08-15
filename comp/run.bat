470fcb369efb029090c701fa58da0085dc109cb7
Dwk0910 <66874914+Dwk0910@users.noreply.github.com>
2023-08-15 09:28:11 +0900

    Update run.bat

--- comp/run.bat
+++ comp/run.bat
@@ -1,2 +1,2 @@
-javac ./../StudentManager/*.java -ending utf-8 -d ./../classes && cd ./../classes && java StudentManager.Main
+javac ./../StudentManager/*.java -encoding utf-8 -d ./../classes && cd ./../classes && java StudentManager.Main
 pause
